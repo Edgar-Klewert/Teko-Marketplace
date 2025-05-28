44da06fd9d55eeeebb6fae5831698cb67a97c045
GustavoTe1xeira <gustavoteixeirabdeoliveira@outlook.com>
2025-05-27 21:37:56 -0300

    feat: Config firebase w/ Auth, firestrore and storage

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -1,19 +1,21 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+//config firebase w/ AUTH, FIRESTORE AND STORAGE
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-
-const clientcredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-if (!firebase.getApps.length) {
-  firebase.initializeApp(clientcredentials);
-}
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export default firebase;
+export { app, auth, db, storage };
